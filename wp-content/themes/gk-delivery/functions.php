271e30c74ba286898f19ac3db021015a0e5fa3a9
Albina0304 <aaa.aniskevich@gmail.com>
2023-06-22 16:45:50 +0200

    Section labor

--- wp-content/themes/gk-delivery/functions.php
+++ wp-content/themes/gk-delivery/functions.php
@@ -1,5 +1,13 @@
 <?php 
 add_theme_support( 'menus' );
+// if ( ! function_exists( 'gk_register_nav_menu' ) ) {
+//     function power_register_nav_menu(){
+// 		register_nav_menus( array(
+// 	    	'header_menu' => __( 'Header Menu'),
+// 		) ); 
+// 	}
+// 	add_action( 'after_setup_theme', 'gk_register_nav_menu' );
+// }
     function my_scripts() {
         wp_enqueue_script( 'main_js', get_template_directory_uri()."/dist/js/main.js", array(), '', true);
         wp_enqueue_script( 'jQuery', "https://code.jquery.com/jquery-3.6.3.js");
@@ -18,4 +26,10 @@ add_theme_support( 'menus' );
     }
     add_action('wp_enqueue_scripts', 'my_scripts');
     add_image_size( 'primary', 282, 126 );
+    // add_image_size( 'sm-img', 80, 80 );
+    // add_image_size( 'custom-size', 596, 448 );
+    // add_image_size( 'main-size', 517, 517 );
+    // add_filter('wp_nav_menu_container_allowedtags', function($args) {
+    //     return array('nav');
+    // })
 ;?>
--- wp-content/themes/gk-delivery/index.php
+++ wp-content/themes/gk-delivery/index.php
@@ -95,4 +95,53 @@
         </div>
     </div>
 </section>
-
+<section class="section labor">
+    <div class="container">
+        <div class="labor-global">
+            <?php $labor_title = get_field('labor_title', 'options');?>
+            <div class="labor-discription">
+                <h3 class="labor-title">
+                    <?php echo $labor_title;?>
+                </h3>
+                <?php $labor_text = get_field('labor_text', 'options');?>
+                <div class="labor-text">
+                    <?php echo $labor_text;?>
+                </div>
+            </div>
+            <div class="labor-colons-slider">
+                <?php $labors_repeater = get_field('labor_repeater', 'options');?>
+                    <?php foreach($labors_repeater as $labor_repeater) { ?>
+                    <div class="labor-col">
+                        <div class="labor-col-card">
+                            <div class="labor-col-img">
+                                <?php echo wp_get_attachment_image($labor_repeater['image']['ID'], 'primary') ;?>
+                            </div>
+                            <div class="labor-col-info">
+                                <div class="number">
+                                    <?php echo ($labor_repeater['number']) ;?>
+                                </div>
+                                <h4 class="labor-col-title">
+                                    <?php echo ($labor_repeater['title']) ;?>
+                                </h4>
+                                <div class="labor-col-text">
+                                    <?php echo ($labor_repeater['text']) ;?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ;?>
+            </div>
+            <div class="section-button">
+                <?php 
+                $link = get_field('btn_primary');
+                    if( $link ): 
+                    $link_target = $link['target'] ? $link['target'] : '_self';?>
+                    <a class="btn btn-primary" href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['title'] ); ?>">
+                        <?php echo esc_html( $link['title'] ); ?>
+                    </a>
+                <?php endif; ?>
+            </div>
+        </div>   
+    </div>
+</section>
+<?php get_footer()?>
